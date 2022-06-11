3a1ac095ebc793b1adbe517597ba87cdef03f2e9
Xsufu <aluno.macarrao@gmail.com>
2022-06-11 16:36:15 +0700

    doWork() настроена на работу с внешним URI

--- app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -2,11 +2,12 @@ package com.example.background.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.text.TextUtils
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.example.background.KEY_IMAGE_URI
-import com.example.background.R
 
 private const val TAG = "BlurWorker"
 class BlurWorker(ctx: Context, params: WorkerParameters): Worker(ctx, params) {
@@ -18,20 +19,25 @@ class BlurWorker(ctx: Context, params: WorkerParameters): Worker(ctx, params) {
         makeStatusNotification("Blurring image", appContext)
 
         return try {
-            val picture = BitmapFactory.decodeResource(
-                appContext.resources,
-                R.drawable.android_cupcake
+            if (TextUtils.isEmpty(resourceUri)) {
+                Log.e(TAG, "Invalid input uri")
+                throw IllegalArgumentException("Invalid input uri")
+            }
+
+            val resolver = appContext.contentResolver
+
+            val picture = BitmapFactory.decodeStream(
+                resolver.openInputStream(Uri.parse(resourceUri))
             )
 
             val output = blurBitmap(picture, appContext)
 
             val outputUri = writeBitmapToFile(appContext, output)
 
-            makeStatusNotification("Output is $outputUri", appContext)
-
             Result.success()
         } catch (throwable: Throwable) {
             Log.e(TAG, "Error applying blur")
+            throwable.printStackTrace()
             Result.failure()
         }
     }
